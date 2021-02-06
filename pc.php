537b818dacdd3faa6ce576eda203d76d28a76828
ramonkj <rk0191@hotmail.com>
2021-02-06 12:28:18 -0300

    Fixed Precadastro and minor alterations

---
+++ pc.php
@@ -0,0 +1,40 @@
+<?php
+
+print_r($POST);
+
+$Fullname = ($_POST["fullname"]);
+$Email = ($_POST["email"]);
+$Phone = ($_POST["phone"]);
+$Datnasc = ($_POST["datnasc"]);
+$Usertype = ($_POST["user"]);
+$Profession = ($_POST["profession"]);
+$Otherprof = ($_POST["otherProf"]);
+$Commerce = ($_POST["commerce"]);
+$Othercom = ($_POST["otherCom"]);
+$ComName = ($_POST["commerceName"]);
+
+$hostname="localhost";
+$username="HeevoUser";
+$password="Pi@8545195";
+$dbname="HeevoDB";
+$usertable="precadastro";
+
+$link = mysqli_connect($hostname,$username,$password,$dbname);
+
+if($link===false){
+    die("ERROR: Could not connect. " .mysqli_connect_error());
+}
+
+$sql = "INSERT INTO precadastro (fullname,email,phone,datnasc,usertype,profession,otherprof,commerce,othercom,commercename) VALUES ('$Fullname','$Email','$Phone','$Datnasc','$Usertype','$Profession','$Otherprof', '$Commerce','$Othercom','$ComName')";
+
+if(mysqli_query($link,$sql)){
+   header("Location:https://www.heevo.life");
+}
+else{
+   echo "ERROR: $sql . " .  mysqli_error($link);
+}
+
+mysqli_close();
+
+header('Location ../index.html');
+?>
