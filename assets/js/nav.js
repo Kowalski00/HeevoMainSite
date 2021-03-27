8a629a3fe8787a75a6f651b5b8b6fff7e7342c68
ramonkj <rk0191@hotmail.com>
2021-03-27 16:39:50 -0300

    Some adjusts on fontsizes and margins

--- assets/js/nav.js
+++
@@ -1,8 +0,0 @@
-//Responsive navbar 
-const navLinks = document.querySelector(".nav-links");
-const hamb = document.querySelector("i.hamb");
-
-hamb.addEventListener("click", () => {
-    navLinks.classList.toggle("menu-mobile")
-})
-
--- assets/js/prontuario.js
+++
@@ -1,4 +0,0 @@
-const active = document.querySelector(".active")
-if(active.classList.contains("active")) {
-    active.style.backgroundColor = "#042a49"
-}
